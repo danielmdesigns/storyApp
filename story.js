640bb3c3f9d9f6a9af56a775ee439cd83a138b7c
danielmdesigns <danielwmaldonado@gmail.com>
2015-02-26 09:40:23 -0600

    Initial Submit
    Story App Game (@TeamTreehouse)

---
+++ story.js
@@ -0,0 +1,17 @@
+var name = prompt("To get started, what's your name?");
+var message = "Hi " + name;
+
+message = message + "! So the story goes - you're deep in the forest and it's dark w/ only the moonlight to guide you and a wolf jumps out to eat you! So scared you ";
+document.write(message)
+
+var attack = prompt("Ok, what would you do if you were walking a bear attacked you?");
+var message2 = attack + " and he dies!";
+
+message = message2
+document.write(message2)
+
+var getAway = prompt("...and what if you were being attacked by hundreds of bees?");
+var message3 = " Unfortunately, for you, vultures begin to circle and they'll eat you too. What are you going to do now? " + getAway + "? But.... that wouldn't actually happen. TO BE CONTINUED...";
+
+message2 = message3
+document.write(message3)
